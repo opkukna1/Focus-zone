17b3bb703cab3cc034321bfe42609b172c9e64f8
opkukna1 <Opsiddh41@gmail.com>
2026-05-28 14:36:24 +0530

    Create AppInfo.kt

---
+++ ​app/src/main/java/com/focuskavach/data/AppInfo.kt
@@ -0,0 +1,10 @@
+package com.focuskavach.data
+
+import android.graphics.drawable.Drawable
+
+data class AppInfo(
+    val packageName: String,
+    val appName: String,
+    val icon: Drawable,
+    var isSelected: Boolean = false
+)
